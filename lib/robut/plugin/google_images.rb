2e88cc5c1efcc22ecb2ee67a03c620dbf1399576
Bernardo Galindo <bernardo466@gmail.com>
2013-11-26 17:19:10 -0600

    update google images to handle format

--- lib/robut/plugin/google_images.rb
+++ lib/robut/plugin/google_images.rb
@@ -4,14 +4,19 @@ require 'google-search'
 class Robut::Plugin::GoogleImages
   include Robut::Plugin
 
-  desc "image <query> - responds with the first image from a Google Images search for <query>"
-  match /^image (.*)/, :sent_to_me => true do |query|
-    image = Google::Search::Image.new(:query => query).first
+  def usage
+    "image <query> - responds with the first image from a Google Images search for <query>"
+  end
+  
+  def handle(query)
+    match /^image (.*)/, :sent_to_me => true do |query|
+      image = Google::Search::Image.new(:query => query).first
 
-    if image
-      reply image.uri
-    else
-      reply "Couldn't find an image"
+      if image
+        reply image.uri
+      else
+        reply "Couldn't find an image"
+      end
     end
   end
 end
