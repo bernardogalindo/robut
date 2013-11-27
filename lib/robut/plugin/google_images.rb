cc08b88c65564c4944726aad441f4dc54109b763
Bernardo Galindo <bernardo466@gmail.com>
2013-11-26 18:11:21 -0600

    rollback

--- lib/robut/plugin/google_images.rb
+++ lib/robut/plugin/google_images.rb
@@ -4,19 +4,15 @@ require 'google-search'
 class Robut::Plugin::GoogleImages
   include Robut::Plugin
 
-  def usage
-    "image <query> - responds with the first image from a Google Images search for <query>"
-  end
-  
-  def handle(time, sender_nick, message)
-    message.match /^image (.*)/, :sent_to_me => true do |query|
-      image = Google::Search::Image.new(:query => query).first
+  desc "image <query> - responds with the first image from a Google Images search for <query>"
 
-      if image
-        reply image.uri
-      else
-        reply "Couldn't find an image"
-      end
+  match /^image (.*)/, :sent_to_me => true do |query|
+    image = Google::Search::Image.new(:query => query).first
+
+    if image
+      reply image.uri
+    else
+      reply "Couldn't find an image"
     end
   end
 end
